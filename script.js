3339a25694ebe1d69b54678e8d46391e8cfba754
mdj10 <183883653+mdj10@users.noreply.github.com>
2025-02-19 07:23:24 -0500

    Create checkWinner function

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function getComputerChoice () {
     const choice = options[Math.floor(Math.random() * options.length)]
     return choice;
 }
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissor") || 
+        (playerSelection === "scissor" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ){
+        return "Player Won";
+    } else {
+        return "Computer Won";
+    }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
