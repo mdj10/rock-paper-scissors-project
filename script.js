1a90e56b2db9a9f0dc11e83e2dfb746623c59f69
mdj10 <183883653+mdj10@users.noreply.github.com>
2025-02-19 08:27:54 -0500

    Create playerSelection and playGame functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const options = ["rock", "paper", "scissors"];
 
-function getComputerChoice () {
+function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)]
     return choice;
 }
@@ -19,7 +19,6 @@ function checkWinner(playerSelection, computerSelection) {
     }
 }
 
-
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection);
     
@@ -31,3 +30,54 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
+
+function getPlayerChoice() {
+    let validatedInput = false;
+
+    while (validatedInput === false) {
+        const choice = prompt("Rock, Paper or Scissors?");
+
+        if (choice === null) {
+            continue;
+        }
+
+        const choiceInLower = choice.toLowerCase();
+
+        if (options.includes(choiceInLower)) {
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+function game() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    console.log("Welcome");
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("New Round");
+
+        if (checkWinner(playerSelection, computerSelection) === "Player") {
+            humanScore++;
+        } else if (checkWinner(playerSelection, computerSelection) === "Computer") {
+            computerScore++;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Player was the winner!");
+    } else if (humanScore < computerScore) {
+        console.log("Computer was the winner!");
+    } else {
+        console.log("It's a ie!");
+    }
+
+    console.log("Game Over");
+}
+
+game();
